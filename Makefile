bc3b3f516c5ddd0bc18ae014a26e6da1d8deb8bf
TheoVerhelst <tverhels@ulb.ac.be>
2016-12-30 02:34:55 +0100

    Use Boost to handle command-line options easily

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:main.cpp
-	g++ main.cpp Grid.cpp Cell.cpp GameBoard.cpp -lsfml-system -lsfml-window -lsfml-graphics -Ofast -Wall -Wextra -pg -std=c++14
+	g++ main.cpp Grid.cpp Cell.cpp GameBoard.cpp -I/home/theo/cpp/lib/boost_1_62_0 -L/home/theo/cpp/lib/boost_1_62_0/stage/lib -lsfml-system -lsfml-window -lsfml-graphics -lboost_program_options -Ofast -Wall -Wextra -std=c++14
--- main.cpp
+++ main.cpp
@@ -1,23 +1,43 @@
 #include <iostream>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
 #include <SFML/Graphics.hpp>
 #include "Grid.hpp"
 #include "GameBoard.hpp"
 
+
+namespace program_options = boost::program_options;
+
 int main(int argc, char** argv)
 {
 	// Default parameters
-	sf::Time updateTime{sf::seconds(0.1)};
-	double aliveProbability{0.2};
+	float updateTimeDouble;
+	double aliveProbability;
 	std::size_t height{50}, width{50};
 	float cellSize{10.f};
 
-	if(argc > 3)
-		return 1;
-	if(argc > 2)
-		aliveProbability = std::stod(argv[2]);
-	if(argc > 1)
-		updateTime = sf::seconds(std::stod(argv[1]));
+	// Declare the supported options.
+	program_options::options_description options("Allowed options");
+	options.add_options()
+		("help,h", "show this help and exit")
+		("alive-probability,p", program_options::value<double>(&aliveProbability)->default_value(.4),
+			"set probability that initial cell is alive")
+		("update-time,t", program_options::value<float>(&updateTimeDouble)->default_value(.1f),
+			"minimum time between two updates")
+	;
 
+	program_options::variables_map variablesMap;
+	program_options::store(program_options::parse_command_line(argc, argv, options), variablesMap);
+	program_options::notify(variablesMap);
+
+	if(variablesMap.count("help"))
+	{
+		std::cout << options << std::endl;
+		return 0;
+	}
+
+	sf::Time updateTime{sf::seconds(updateTimeDouble)};
     sf::RenderWindow window(sf::VideoMode(cellSize * width, cellSize * height), "The Great Game of Life");
 
 	Grid grid{height, width, aliveProbability};
