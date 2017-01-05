0464a638e5c796abe06eeddd78ed3b7749da1c47
TheoVerhelst <tverhels@ulb.ac.be>
2017-01-05 01:09:38 +0100

    Fix grid modulo neighbour check

--- src/Cell.cpp
+++ src/Cell.cpp
@@ -31,14 +31,26 @@ const State& Cell::getState() const
 
 std::size_t Cell::countAliveNeighbours(const Grid& grid, std::size_t row, std::size_t col) const
 {
-	const int gridHeight{static_cast<int>(grid.getHeight())}, gridWidth{static_cast<int>(grid.getWidth())};
-	std::array<std::pair<int, int>, 8> neighboursToCheck{{
+	const int gridHeight{static_cast<int>(grid.getHeight())},
+		gridWidth{static_cast<int>(grid.getWidth())},
+		intRow{static_cast<int>(row)},
+		intCol{static_cast<int>(col)};
+	const std::array<std::pair<int, int>, 8> neighboursToCheck{{
 		{-1, -1}, {-1, 0}, {-1, 1},
 		{ 0, -1},          { 0, 1},
 		{ 1, -1}, { 1, 0}, { 1, 1}}};
 	std::size_t res{0UL};
 	for(auto& neighbour : neighboursToCheck)
-		if(grid.getState((row + neighbour.first)  % gridHeight, (col + neighbour.second) % gridWidth) == State::Alive)
+	{
+		int i{(intRow + neighbour.first) % gridHeight},
+			j{(intCol + neighbour.second) % gridWidth};
+		if(i < 0)
+			i += gridHeight;
+		if(j < 0)
+			j += gridWidth;
+
+		if(grid.getState(i, j) == State::Alive)
 			++res;
+	}
 	return res;
 }
